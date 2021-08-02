9fe06ef1b469abb5c28e446fd9e3500d567d0c2d
KutuevVladimir <VladimirKutuev@gmail.com>
2021-08-02 10:14:50 +0300

    Prepared scripts for running benchmarks on aws ec2

---
+++ install.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+set -e
+
+download_dataset()
+{
+  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=2" -O- \
+  | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$1" -O "$2" \
+  && rm -rf /tmp/cookies.txt
+}
+
+# shellcheck source=./BENCHMARKS.config
+. BENCHMARKS.config
+
+# Git, Python, Pip
+echo "Installing wget, git, python3, pip3"
+apt update
+apt upgrade -y
+apt install wget git python3 python3-pip -y
+
+# SuiteSparse:GraphBLAS
+echo "Installing SuiteSparse:GraphBLAS"
+SS_BURBLE=0
+SS_COMPACT=0
+
+git clone --branch "${SS_RELEASE}" --single-branch https://github.com/DrTimothyAldenDavis/GraphBLAS.git
+cd GraphBLAS/build
+cmake .. -DGB_BURBLE="${SS_BURBLE}" -DGBCOMPACT="${SS_COMPACT}"
+make "-j$(nproc)"
+sudo make install
+cd ../..
+
+# pygraphblas
+echo "Installing pygraphblas"
+
+git clone --branch "${PYGRAPHBLAS_VERSION}" --single-branch https://github.com/Graphegon/pygraphblas.git
+cd pygraphblas
+pip3 install -r notebook-requirements.txt
+python3 setup.py install --user
+cd ..
+
+# CFPQ_PyAlgo
+echo "Installing CFPQ_PyAlgo"
+
+git clone --branch "${CFPQ_BRANCH}" --single-branch --recurse-submodules https://github.com/JetBrains-Research/CFPQ_PyAlgo.git
+cd CFPQ_PyAlgo
+
+echo "Installing deps/"
+cd deps/CFPQ_Data
+pip3 install -r requirements.txt
+python3 setup.py install --user
+
+cd ../../
+pip3 install -r requirements.txt
+
+echo "Test CFPQ_PyAlgo"
+python3 -m pytest test -v -m "CI"
+
+# Dataset
+echo "Preparing benchmark dataset"
+
+cd benchmark
+download_dataset "${SMALL_DATASET_FILE_ID}" "${SMALL_DATASET_FILE_NAME}"
+tar -xvf "${SMALL_DATASET_FILE_NAME}"
+download_dataset "${LARGE_DATASET_FILE_ID}" "${LARGE_DATASET_FILE_NAME}"
+tar -xvf "${LARGE_DATASET_FILE_NAME}"
---
+++ run_benchmark.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# shellcheck source=./BENCHMARKS.config
+. BENCHMARKS.config
+
+cd CFPQ_PyAlgo || return 1
+INSTANCE_CORES_THREADS="$(cat /proc/cpuinfo | grep "cores" |uniq|awk '{print $4}')_$(cat /proc/cpuinfo | grep -c "processor")"
+
+mkdir -p "results/small_${INSTANCE_CORES_THREADS}"
+echo "Benchmarking SMALL_GRAPHS" && python3 -m benchmark.start_benchmark -ms_algo "${MS_ALGORITHM}" -data_dir "${SMALL_GRAPHS_DATASET_PATH}" -all_pairs_rounds "${SMALL_GRAPHS_ALL_PAIRS_ROUNDS}" -ms_worst_rounds "${SMALL_GRAPHS_MS_WORST_ROUNDS}" -ms_min_reachable_vertices "${SMALL_GRAPHS_MS_MIN_REACHABLE_VERTICES}" -ms_worst_count "${SMALL_GRAPHS_MS_WORST_VERTICES_COUNT}" -result_dir "results/small_${INSTANCE_CORES_THREADS}"
+
+mkdir -p "results/large_${INSTANCE_CORES_THREADS}"
+echo "Benchmarking LARGE_GRAPHS" && python3 -m benchmark.start_benchmark -ms_algo "${MS_ALGORITHM}" -data_dir "${LARGE_GRAPHS_DATASET_PATH}" -all_pairs_rounds "${LARGE_GRAPHS_ALL_PAIRS_ROUNDS}" -ms_worst_rounds "${LARGE_GRAPHS_MS_WORST_ROUNDS}" -ms_min_reachable_vertices "${LARGE_GRAPHS_MS_MIN_REACHABLE_VERTICES}" -ms_worst_count "${LARGE_GRAPHS_MS_WORST_VERTICES_COUNT}" -result_dir "results/large_${INSTANCE_CORES_THREADS}"
+
