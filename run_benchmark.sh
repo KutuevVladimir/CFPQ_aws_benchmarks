2f56426f41b133b4e7e162d5df749b838e28b2a9
KutuevVladimir <VladimirKutuev@gmail.com>
2021-08-08 18:29:53 +0300

    Fixed ImportError: libgraphblas.so for run_benchmark script

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -6,6 +6,8 @@
 cd CFPQ_PyAlgo || return 1
 INSTANCE_CORES_THREADS="$(cat /proc/cpuinfo | grep "cores" |uniq|awk '{print $4}')_$(cat /proc/cpuinfo | grep -c "processor")"
 
+export LD_LIBRARY_PATH="/usr/local/lib:${LD_LIBRARY_PATH}"
+
 mkdir -p "results/small_${INSTANCE_CORES_THREADS}"
 echo "Benchmarking SMALL_GRAPHS" && python3 -m benchmark.start_benchmark -ms_algo "${MS_ALGORITHM}" -data_dir "${SMALL_GRAPHS_DATASET_PATH}" -all_pairs_rounds "${SMALL_GRAPHS_ALL_PAIRS_ROUNDS}" -ms_worst_rounds "${SMALL_GRAPHS_MS_WORST_ROUNDS}" -ms_min_reachable_vertices "${SMALL_GRAPHS_MS_MIN_REACHABLE_VERTICES}" -ms_worst_count "${SMALL_GRAPHS_MS_WORST_VERTICES_COUNT}" -result_dir "results/small_${INSTANCE_CORES_THREADS}"
 
